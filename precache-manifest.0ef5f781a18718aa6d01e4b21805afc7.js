6b188e73eff1945c42a392bce6528199fc7d5a43
Shrey <shreybirmiwal@gmail.com>
2022-05-29 23:12:54 -0500

    Updates

---
+++ precache-manifest.0ef5f781a18718aa6d01e4b21805afc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aec1425735abefb3715",
+    "url": "/robotfriends/static/css/main.aa8e767f.chunk.css"
+  },
+  {
+    "revision": "9aec1425735abefb3715",
+    "url": "/robotfriends/static/js/main.9aec1425.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/robotfriends/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "1be09bbb901518212698",
+    "url": "/robotfriends/static/css/2.8989cbf6.chunk.css"
+  },
+  {
+    "revision": "1be09bbb901518212698",
+    "url": "/robotfriends/static/js/2.1be09bbb.chunk.js"
+  },
+  {
+    "revision": "8a9ad1ba5174f448d646",
+    "url": "/robotfriends/static/js/runtime~main.8a9ad1ba.js"
+  },
+  {
+    "revision": "f275defb04bb5b5a0ce735e5d4a8a596",
+    "url": "/robotfriends/index.html"
+  }
+];
